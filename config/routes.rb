611b1df618007fdd9ad2e09442edf272056f87dd
asanebou.mackey <asanebou.job4219@gmail.com>
2023-09-08 15:02:40 +0900

    get homes/index をルートパス root homes#index に変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  root 'homes#index'
   devise_for :users
-  get 'homes/index'
+  # get 'homes/index' をルートパス root 'homes#index' に変更
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
