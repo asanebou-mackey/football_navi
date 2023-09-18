37cd276ad3cc48bae8cf63af9a844f204ff54017
asanebou.mackey <asanebou.job4219@gmail.com>
2023-09-18 16:08:52 +0900

    Userテーブルにnameカラムを追加、deviseのフラッシュメッセージを表示、ナビゲーションバーをfixed-topからsticky-topに変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_25_014916) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_12_013150) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -31,6 +31,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_25_014916) do
     t.datetime "locked_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.string "introduction"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
