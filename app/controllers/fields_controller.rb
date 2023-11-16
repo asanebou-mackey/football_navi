dda4ab835bac33be38b9039a030316dbea91e766
asanebou.mackey <asanebou.job4219@gmail.com>
2023-11-16 19:24:40 +0900

    コート登録にバリデーションを実装

--- app/controllers/fields_controller.rb
+++ app/controllers/fields_controller.rb
@@ -13,7 +13,7 @@ class FieldsController < ApplicationController
       flash[:notice] = "コートを登録しました"
       redirect_to root_path
     else
-      render "new"
+      render :new, status: :unprocessable_entity
     end
   end
 
--- app/models/field.rb
+++ app/models/field.rb
@@ -1,3 +1,8 @@
 class Field < ApplicationRecord
   belongs_to :user
+
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :number, presence: true
+  validates :price, presence: true
 end
