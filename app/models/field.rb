169c3febd0005ea2b3a2ac7d21c61eee62b2ab31
asanebou.mackey <asanebou.job4219@gmail.com>
2023-12-05 17:59:08 +0900

    画像にバリデーションを実装

--- app/models/field.rb
+++ app/models/field.rb
@@ -7,4 +7,18 @@ class Field < ApplicationRecord
   validates :address, presence: true
   validates :number, presence: true
   validates :price, presence: true
+  validate :image_content_type
+  validate :image_size
+
+  def image_content_type
+    if image.attached? && !image.content_type.in?(%w[image/jpeg image/png image/gif])
+      errors.add(:image, '：ファイル形式が、JPEG, PNG, GIF以外になってます。ファイル形式をご確認ください。')
+    end
+  end
+
+  def image_size
+    if image.attached? && image.blob.byte_size > 1.megabytes
+      errors.add(:image, '：1MB以下のファイルをアップロードしてください。')
+    end
+  end
 end
